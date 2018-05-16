454e9f0f131613e9deaa40d88c6591012c47efd9
Tidjini Messaoudi <tidjini.messaoudi@gmail.com>
2018-05-16 22:55:02 +0100

    add bluprint staff

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import { Classes, Intent, Spinner, Button } from "@blueprintjs/core";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -13,6 +14,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Spinner className={Classes.SMALL} intent={Intent.PRIMARY} />
+        <Button icon="edit" rightIcon="caret-down" text="Edit" />
       </div>
     );
   }
