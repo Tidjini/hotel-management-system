66e82644ad1f493339968458520f3c88021afcc7
Tidjini Messaoudi <tidjini.messaoudi@gmail.com>
2018-05-17 23:13:32 +0100

    Add some config for redux store

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,33 +1,8 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import axios from "axios";
-import { Classes, Intent, Spinner, Button } from "@blueprintjs/core";
-import { Cell, Column, Table } from "@blueprintjs/table";
 import "./App.css";
 
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/"
-});
-
 class App extends Component {
-  cellRenderer = rowIndex => {
-    return <Cell>{`$${(rowIndex * 10).toFixed(2)}`}</Cell>;
-  };
-
-  componentWillMount() {
-    const res = axiosInstance.get("/guests").then(data => {
-      console.log(
-        data.map(([firstname, lastname, age, cartId, phoneNumber, autre]) => ({
-          firstname,
-          lastname,
-          age,
-          cartId,
-          phoneNumber,
-          autre
-        }))
-      );
-    });
-  }
   render() {
     return (
       <div className="App">
@@ -35,21 +10,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Spinner className={Classes.SMALL} intent={Intent.PRIMARY} />
-        <Button icon="edit" rightIcon="caret-down" text="Edit" />
-        <Table
-          numRows={10}
-          enableColumnReordering={true}
-          enableColumnResizing={false}
-          enableRowReordering={true}
-          enableRowResizing={false}
-        >
-          <Column name="Dollars" cellRenderer={this.cellRenderer} />
-          <Column name="Euro" cellRenderer={this.cellRenderer} />
-        </Table>
       </div>
     );
   }
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+import reducers from "./reducers";
+//axios for custom client axios instance
+import axios from "axios";
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:5000/api"
+});
+
+const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(thunk.withExtraArgument(axiosInstance))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
