428b1a8c4ebb1c72944f32f8fd05fae038836f6c
Tidjini Messaoudi <tidjini.messaoudi@gmail.com>
2018-05-30 19:58:51 +0100

    Add Chambres get  requet in routes/ChambreRouteHandler

--- server/routes/ChambreRouteHandler.js
+++ server/routes/ChambreRouteHandler.js
@@ -2,7 +2,7 @@ const { Chambre } = require("./../models/Chambre");
 
 module.exports = app => {
   //post the chambre request
-  app.post("/api/chambre", (req, res) => {
+  app.post("/api/chambres", (req, res) => {
     const chambre = new Chambre({
       num: req.body.num,
       type: req.body.type,
@@ -16,6 +16,19 @@ module.exports = app => {
     chambre.save().then(
       doc => {
         res.send(doc);
+      },
+      err => {
+        res.status(400).send(err);
+      }
+    );
+  });
+
+  //get Chamber List
+  app.get("/api/chambres", (req, res) => {
+    // apply the fetch of { _application_key: req.app.key, _user_role: req.user.role }
+    Chambre.find().then(
+      chambres => {
+        res.send({ chambres });
       },
       err => {
         res.status(400).send(err);
