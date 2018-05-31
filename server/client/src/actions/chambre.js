62248d3ac1466430424febe5f95b9c0482871ed4
Tidjini Messaoudi <tidjini.messaoudi@gmail.com>
2018-05-31 18:39:17 +0100

    delete/update chambre action

--- server/client/src/actions/chambre.js
+++ server/client/src/actions/chambre.js
@@ -14,10 +14,25 @@ export const fetchChambres = () => async (dispatch, getState, api) => {
 };
 
 export const addChambre = chambre => async (dispatch, getState, api) => {
-  console.log(api);
   const res = await api.post("/chambres", chambre);
   dispatch({
     type: SAVE_CHAMBRE,
+    payload: res
+  });
+};
+
+export const deleteChambre = chambreId => async (dispatch, getState, api) => {
+  const res = await api.delete("/chambres/" + chambreId);
+  dispatch({
+    type: DELETE_CHAMBRE,
+    payload: res
+  });
+};
+
+export const updateChambre = chambre => async (dispatch, getState, api) => {
+  const res = await api.patch("/chambres/" + chambre._id);
+  dispatch({
+    type: UPDATE_CHAMBRE,
     payload: res
   });
 };
