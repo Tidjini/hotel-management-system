fed773d83a958e2835848f27219851d84d825847
Tidjini Messaoudi <tidjini.messaoudi@gmail.com>
2018-05-31 18:54:43 +0100

    add chambre/chambreCollecion

--- server/client/src/reducers/index.js
+++ server/client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import GuestsReducer from "./guestsReducer";
 import AddGuest from "./addGuest";
 import deleteReducer from "./deleteReducer";
+import chambreReducer from "./chambreReducer";
 
 export default combineReducers({
   guests: GuestsReducer,
   newGuest: AddGuest,
-  guestDeleted: deleteReducer
+  guestDeleted: deleteReducer,
+  chambresData: chambreReducer
 });
