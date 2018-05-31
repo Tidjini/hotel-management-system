398862d127650a323b2143a71afc30f687ecc4d4
Tidjini Messaoudi <tidjini.messaoudi@gmail.com>
2018-05-31 19:07:06 +0100

    add antd in package

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,22 +1,10 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import "./App.css";
-import GuestPage from "./components/GuestPage";
-import HeaderComponent from "./components/HeaderComponent";
-import LeftMenuComponent from "./components/LeftMenuComponent";
-// import ActionTabsComponent from "./components/ActionTabsComponent";
+import "antd/dist/antd.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <HeaderComponent />
-        <div>
-          <LeftMenuComponent />
-          <GuestPage />
-        </div>
-      </div>
-    );
+    return <div className="App" />;
   }
 }
 
