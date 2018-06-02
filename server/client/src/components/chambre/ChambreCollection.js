fbba673f815a8afddc5d886bd3ef985a639f626f
Tidjini Messaoudi <tidjini.messaoudi@gmail.com>
2018-06-02 19:22:14 +0100

    Add Chambre collection data

--- server/client/src/components/chambre/ChambreCollection.js
+++ server/client/src/components/chambre/ChambreCollection.js
@@ -1,16 +1,62 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class ChambreCollection extends Component {
+import { Table } from "antd";
+
+const columns = [
+  {
+    title: "Numéro",
+    dataIndex: "num",
+    key: "num"
+  },
+  {
+    title: "Type",
+    dataIndex: "type",
+    key: "type"
+  },
+  {
+    title: "Vue",
+    dataIndex: "vue",
+    key: "vue"
+  },
+  {
+    title: "Etat",
+    dataIndex: "etat",
+    key: "etat"
+  },
+  {
+    title: "Nombre Lits",
+    dataIndex: "nombreLit",
+    key: "nombreLit"
+  },
+  {
+    title: "Prix",
+    dataIndex: "price",
+    key: "price"
+  }
+];
+
+class ChambreCollection extends React.Component {
+  componentWillMount() {
+    console.log(this.props.chambres);
+  }
   render() {
     return (
-      <div>
-        <h1>Chambre ChambreCollection</h1>
-        <h1>Chambre ChambreCollection</h1>
-        <h1>Chambre ChambreCollection</h1>
-        <h1>Chambre ChambreCollection</h1>
-        <h1>Chambre ChambreCollection</h1>
-        <h1>Chambre ChambreCollection</h1>
-      </div>
+      <Table
+        bordered
+        size="middle"
+        dataSource={this.props.chambres}
+        columns={columns}
+      />
     );
   }
 }
+
+const mapStateToProps = state => {
+  const { chambres } = state.chambresData;
+  return {
+    chambres
+  };
+};
+
+export default connect(mapStateToProps, null)(ChambreCollection);
