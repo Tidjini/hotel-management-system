4bc48eac31e802ea5add3c92a323261c751b35d0
Tidjini_Messaoudi <dy.31@hotmail.fr>
2018-06-05 14:55:01 +0100

    Add chambre with validation modal

--- server/client/src/components/chambre/ChambreView.js
+++ server/client/src/components/chambre/ChambreView.js
@@ -4,6 +4,7 @@ import { Form, Row, Col, Input, Button, Icon, InputNumber, Card } from "antd";
 
 import { connect } from "react-redux";
 import { addChambre, fetchChambres } from "./../../actions";
+import ConfirmationModal from "./../common/ConfirmationModal";
 
 const FormItem = Form.Item;
 
@@ -16,29 +17,45 @@ class ChambreView extends React.Component {
     etat: this.props.etat || "",
     nombreLit: this.props.nombreLit || 1,
     nombreLit: this.props.nombreLit || 1,
-    price: this.props.price || 0
+    price: this.props.price || 0,
+    showConfirmation: false,
+    chambre: {}
   };
 
   handleAddChambre = e => {
     e.preventDefault();
-    // this.props.form.setFieldsValue({
-    //   etat: "val",
-    //   nombreLit: 12,
-    //   price: 12312,
-    //   num: 12,
-    //   type: "Type 001"
-    // });
+    // this.props.form.setFieldsValue({ });
+
     this.props.form.validateFields((err, values) => {
-      console.log("Received values of form: ", values);
+      //console.log("Received values of form: ", values);
+      if (err) {
+        return;
+      }
       const chambre = {
         ...values,
         etat: Number(values.etat),
         nombreLit: Number(values.nombreLit),
         price: Number(values.price)
       };
-      console.log(chambre);
-      this.props.addChambre(chambre);
-      this.props.fetchChambres();
+
+      this.setState({
+        showConfirmation: true,
+        chambre
+      });
+
+      //this.props.addChambre(chambre);
+      //this.props.fetchChambres();
+    });
+  };
+
+  handleAdd_Chambre = () => {
+    this.props.addChambre(this.state.chambre);
+    this.props.fetchChambres();
+    this.handleReset();
+  };
+  handleCancel = () => {
+    this.setState({
+      showConfirmation: false
     });
   };
 
@@ -56,108 +73,109 @@ class ChambreView extends React.Component {
     const { formStyle } = styles;
     const { getFieldDecorator } = this.props.form;
     return (
-      <Form style={formStyle} onSubmit={this.handleAddChambre}>
-        <Row>
-          <Card type="inner" title="Chambre">
-            <Col span={12} style={{ textAlign: "right" }}>
-              <Row>
-                <Col span={12} key="num">
-                  <FormItem label="Numéro" style={{ display: "flex" }}>
-                    {getFieldDecorator("num", {
-                      rules: [
-                        {
-                          required: true,
-                          message: `champs requis`
-                        }
-                      ]
-                    })(<Input placeholder={"numéro"} style={{ flex: 1 }} />)}
-                  </FormItem>
-                </Col>
-                <Col span={12} key="type">
-                  <FormItem label="Type" style={{ display: "flex" }}>
-                    {getFieldDecorator("type", {
-                      rules: [{}]
-                    })(<Input placeholder={"type"} style={{ flex: 1 }} />)}
-                  </FormItem>
-                </Col>
-              </Row>
-              <Row>
-                <Col span={12} key="vue">
-                  <FormItem label="Vue" style={{ display: "flex" }}>
-                    {getFieldDecorator("vue", {
-                      rules: [{}]
-                    })(<Input placeholder={"Vue"} style={{ flex: 1 }} />)}
-                  </FormItem>
-                </Col>
-                <Col span={12} key="etat">
-                  <FormItem label="Etat" style={{ display: "flex" }}>
-                    {getFieldDecorator("etat", {
-                      rules: [{}]
-                    })(
-                      <Input
-                        placeholder={"Etat"}
-                        style={{ flex: 1 }}
-                        type="number"
-                      />
-                    )}
-                  </FormItem>
-                </Col>
-              </Row>
-            </Col>
+      <div>
+        <ConfirmationModal
+          visible={this.state.showConfirmation}
+          content="Voulez vous ajouter cette chambre aux collection des chambre ?"
+          onConfirm={this.handleAdd_Chambre.bind(this)}
+          onCancel={this.handleCancel.bind(this)}
+        />
+        <Form style={formStyle} onSubmit={this.handleAddChambre}>
+          <Row>
+            <Card type="inner" title="Chambre">
+              <Col span={12} style={{ textAlign: "right" }}>
+                <Row>
+                  <Col span={12} key="num">
+                    <FormItem label="Numéro" style={{ display: "flex" }}>
+                      {getFieldDecorator("num", {
+                        rules: [
+                          {
+                            required: true,
+                            message: `champs requis`
+                          }
+                        ]
+                      })(<Input placeholder={"numéro"} style={{ flex: 1 }} />)}
+                    </FormItem>
+                  </Col>
+                  <Col span={12} key="type">
+                    <FormItem label="Type" style={{ display: "flex" }}>
+                      {getFieldDecorator("type", {
+                        rules: [{}]
+                      })(<Input placeholder={"type"} style={{ flex: 1 }} />)}
+                    </FormItem>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col span={12} key="vue">
+                    <FormItem label="Vue" style={{ display: "flex" }}>
+                      {getFieldDecorator("vue", {
+                        rules: [{}]
+                      })(<Input placeholder={"Vue"} style={{ flex: 1 }} />)}
+                    </FormItem>
+                  </Col>
+                  <Col span={12} key="etat">
+                    <FormItem label="Etat" style={{ display: "flex" }}>
+                      {getFieldDecorator("etat", {
+                        rules: [{}]
+                      })(
+                        <Input
+                          placeholder={"Etat"}
+                          style={{ flex: 1 }}
+                          type="number"
+                        />
+                      )}
+                    </FormItem>
+                  </Col>
+                </Row>
+              </Col>
 
-            <Col span={12} style={{ textAlign: "right" }}>
-              <Row>
-                <Col span={12} key="nombreLit">
-                  <FormItem label="Nombre Lits" style={{ display: "flex" }}>
-                    {getFieldDecorator("nombreLit", {
-                      rules: [{}]
-                    })(
-                      <Input
-                        placeholder={"Nombre de lits"}
-                        style={{ flex: 1 }}
-                        type="number"
-                      />
-                    )}
-                  </FormItem>
-                </Col>
-              </Row>
-              <Row>
-                <Col span={12} key="price">
-                  <FormItem label="Prix" style={{ display: "flex" }}>
-                    {getFieldDecorator("price", {
-                      rules: [{}]
-                    })(
-                      <Input
-                        placeholder={"Price"}
-                        style={{ flex: 1 }}
-                        type="number"
-                      />
-                    )}
-                  </FormItem>
-                </Col>
-              </Row>
-            </Col>
-          </Card>
-        </Row>
+              <Col span={12} style={{ textAlign: "right" }}>
+                <Row>
+                  <Col span={12} key="nombreLit">
+                    <FormItem label="Nombre Lits" style={{ display: "flex" }}>
+                      {getFieldDecorator("nombreLit", {
+                        rules: [{}]
+                      })(
+                        <Input
+                          placeholder={"Nombre de lits"}
+                          style={{ flex: 1 }}
+                          type="number"
+                        />
+                      )}
+                    </FormItem>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col span={12} key="price">
+                    <FormItem label="Prix" style={{ display: "flex" }}>
+                      {getFieldDecorator("price", {
+                        rules: [{}]
+                      })(
+                        <Input
+                          placeholder={"Price"}
+                          style={{ flex: 1 }}
+                          type="number"
+                        />
+                      )}
+                    </FormItem>
+                  </Col>
+                </Row>
+              </Col>
+            </Card>
+          </Row>
 
-        <Row>
-          <Col span={24} style={{ textAlign: "right" }}>
-            <Button type="primary" htmlType="submit">
-              Ajouter
-            </Button>
-            <Button style={{ marginLeft: 8 }} onClick={this.handleReset}>
-              Réinitializer
-            </Button>
-            <Button
-              type="danger"
-              style={{ marginLeft: 8 }}
-              onClick={this.handleReset}
-            >
-              Supprimer
-            </Button>
-          </Col>
-        </Row>
-      </Form>
+          <Row>
+            <Col span={24} style={{ textAlign: "right" }}>
+              <Button type="primary" onClick={this.handleAddChambre}>
+                Ajouter
+              </Button>
+              <Button style={{ marginLeft: 8 }} onClick={this.handleReset}>
+                Réinitializer
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </div>
     );
   }
 }
@@ -172,6 +190,9 @@ const styles = {
 };
 
 const ChambreForm = Form.create()(ChambreView);
-export default connect(null, { addChambre, fetchChambres })(ChambreForm);
+export default connect(
+  null,
+  { addChambre, fetchChambres }
+)(ChambreForm);
 
 // export default ChambreForm;
---
+++ server/client/src/components/common/ConfirmationModal.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Modal, Button } from "antd";
+
+export default class ConfimationModal extends React.Component {
+  state = {
+    ModalText: this.props.content,
+    confirmLoading: false
+  };
+
+  handleOk = () => {
+    this.setState({
+      confirmLoading: true
+    });
+    setTimeout(() => {
+      this.setState({
+        confirmLoading: false
+      });
+      this.props.onConfirm();
+      this.props.onCancel();
+    }, 2000);
+  };
+  handleCancel = () => {
+    this.props.onCancel();
+  };
+  render() {
+    const { visible, confirmLoading, ModalText } = this.state;
+    return (
+      <div>
+        <Modal
+          title="Title"
+          visible={this.props.visible}
+          onOk={this.handleOk}
+          confirmLoading={confirmLoading}
+          onCancel={this.handleCancel}
+        >
+          <p>{ModalText}</p>
+        </Modal>
+      </div>
+    );
+  }
+}
