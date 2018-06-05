f540d0f2b5ba94946d825540b020053bd808ad29
Tidjini Messaoudi <tidjini.messaoudi@gmail.com>
2018-06-05 18:55:08 +0100

    Add table de base to menu

--- server/client/src/components/common/LeftMenu.js
+++ server/client/src/components/common/LeftMenu.js
@@ -64,13 +64,24 @@ class LeftMenu extends Component {
             key="sub2"
             title={
               <span>
-                <Icon type="team" />
-                <span>Team</span>
+                <Icon type="appstore-o" />
+                <span>Tables de base</span>
               </span>
             }
           >
-            <Menu.Item key="6">Team 1</Menu.Item>
-            <Menu.Item key="8">Team 2</Menu.Item>
+            <Menu.Item key="FamilleCollection">Familles de produits</Menu.Item>
+            <Menu.Item key="TypeServiceCollection">Type de services</Menu.Item>
+            <Menu.Item key="TypeRestaurationCollection">
+              Type de restauration
+            </Menu.Item>
+            <Menu.Item key="GroupeCollection">Groupes</Menu.Item>
+
+            <Menu.Item key="GrpPrivCollection">Groupes de privileges</Menu.Item>
+            <Menu.Item key="PrivilegesCollection">Privileges</Menu.Item>
+            <Menu.Item key="TypeChambreCollection">Types de chambre</Menu.Item>
+            <Menu.Item key="TarifChambreCollection">
+              Tarifs de chambre
+            </Menu.Item>
           </SubMenu>
         </Menu>
       </Sider>
