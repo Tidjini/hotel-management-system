b867d68bd21815905dd60cb7e26760be07039f00
Tidjini_Messaoudi <dy.31@hotmail.fr>
2018-06-06 09:27:05 +0100

    Add some configuation panes

---
+++ server/client/src/components/configurations/Configuartion.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Tabs } from "antd";
+const TabPane = Tabs.TabPane;
+
+export default class Configurations extends Component {
+  render() {
+    return (
+      <Tabs
+        defaultActiveKey="services"
+        tabPosition="Vertical"
+        style={{ height: 220 }}
+      >
+        <TabPane tab="Services" key="services">
+          Services / famille de produit/ type de service / type de restauration
+        </TabPane>
+        <TabPane tab="Groupe et privileges" key="privileges">
+          Groupe / Groupe de privileges/ privileges
+        </TabPane>
+      </Tabs>
+    );
+  }
+}
