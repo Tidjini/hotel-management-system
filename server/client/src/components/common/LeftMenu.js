176c73da8c7da46b697fec3164487aeb3fa20a42
Tidjini_Messaoudi <dy.31@hotmail.fr>
2018-06-06 09:17:26 +0100

    Add configuration menu item

--- server/client/src/components/common/LeftMenu.js
+++ server/client/src/components/common/LeftMenu.js
@@ -60,29 +60,12 @@ class LeftMenu extends Component {
             <Menu.Item key="ChambreCollection">List des chambre</Menu.Item>
             <Menu.Item key="ChambreView">Ajouter une chambre</Menu.Item>
           </SubMenu>
-          <SubMenu
-            key="sub2"
-            title={
-              <span>
-                <Icon type="appstore-o" />
-                <span>Tables de base</span>
-              </span>
-            }
-          >
-            <Menu.Item key="FamilleCollection">Familles de produits</Menu.Item>
-            <Menu.Item key="TypeServiceCollection">Type de services</Menu.Item>
-            <Menu.Item key="TypeRestaurationCollection">
-              Type de restauration
-            </Menu.Item>
-            <Menu.Item key="GroupeCollection">Groupes</Menu.Item>
-
-            <Menu.Item key="GrpPrivCollection">Groupes de privileges</Menu.Item>
-            <Menu.Item key="PrivilegesCollection">Privileges</Menu.Item>
-            <Menu.Item key="TypeChambreCollection">Types de chambre</Menu.Item>
-            <Menu.Item key="TarifChambreCollection">
-              Tarifs de chambre
-            </Menu.Item>
-          </SubMenu>
+          <Menu.Item key="Configuration">
+            <span>
+              <Icon type="setting" />
+              <span>Configuration</span>
+            </span>
+          </Menu.Item>
         </Menu>
       </Sider>
     );
