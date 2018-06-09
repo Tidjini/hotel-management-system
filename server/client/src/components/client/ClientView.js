c9b481c8a46a87ce8cc827ffe4d4cda2271d85fd
Tidjini Messaoudi <tidjini.messaoudi@gmail.com>
2018-06-09 11:02:40 +0100

    Add and test client view

--- server/client/src/components/client/ClientView.js
+++ server/client/src/components/client/ClientView.js
@@ -4,8 +4,24 @@ import { connect } from "react-redux";
 import { fetchClients, addClient } from "./../../actions";
 import { panes } from "./../../ViewModels/clients/ClientViewModel";
 
-export default class ClientView extends Component {
+class ClientView extends Component {
+  addClient = client => {
+    this.props.addClient(client);
+    this.props.fetchClients();
+  };
   render() {
     return <ViewForm panes={panes} addFormData={this.addClient.bind(this)} />;
   }
 }
+
+const mapStateToProps = state => {
+  const { clients } = state.clientData;
+  return {
+    clients
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchClients, addClient }
+)(ClientView);
